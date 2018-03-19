4b9cdd0e81dad6645b106bee279d316b870e3935
Braise <boonen.bastien@gmail.com>
2018-03-19 22:04:24 +0100

    Navigation
    Add navigation to the application based on categories

--- PartyInvite/SportStoreWebUI/Controllers/ProductController.cs
+++ PartyInvite/SportStoreWebUI/Controllers/ProductController.cs
@@ -18,17 +18,21 @@ namespace SportStoreWebUI.Controllers
             repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
-                Products = repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+                Products = repository.Products.Where(p => category == null || p.Category.Equals(category))
+                                                .OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
                 PagingInfo = new PagingInfo
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
-                }
+                    TotalItems = category == null ?
+                                repository.Products.Count() :
+                                repository.Products.Where(x => x.Category.Equals(category)).Count()
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
--- PartyInvite/SportStoreWebUI/Models/ProductsListViewModel.cs
+++ PartyInvite/SportStoreWebUI/Models/ProductsListViewModel.cs
@@ -10,5 +10,6 @@ namespace SportStoreWebUI.Models
     {
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
